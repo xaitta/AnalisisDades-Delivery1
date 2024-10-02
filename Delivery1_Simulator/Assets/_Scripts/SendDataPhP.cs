34374db9a779de365fa0fbbfd597d2fb1aeb9891
xaitta <xiao.shan.costajussa@estudiantat.upc.edu>
2024-10-02 12:08:41 +0200

    Update SendDataPhP.cs
    Co-Authored-By: Jose Antonio Tur <180555660+joseantoniotur@users.noreply.github.com>
    Co-Authored-By: Lluc <99950000+Llucaieaie@users.noreply.github.com>
    Co-Authored-By: Hacienda69 <fannumero1dehacienda@gmail.com>

--- Delivery1_Simulator/Assets/_Scripts/SendDataPhP.cs
+++ Delivery1_Simulator/Assets/_Scripts/SendDataPhP.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SendDataPhP : MonoBehaviour
 {
-    void Start()
+    void OnEnable()
     {
+        Simulator.OnNewPlayer += NewPlayerAction;
         
     }
 
-    void Update()
+    private void NewPlayerAction(string arg1, string arg2, int arg3, float arg4, DateTime arg5)
     {
-        
+        Debug.Log("Hola");
+
+        CallbackEvents.OnAddPlayerCallback?.Invoke(99);
     }
 }
